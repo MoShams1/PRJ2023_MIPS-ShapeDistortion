308795e7ef9d028b423abdeb97b0ff2655e79b71
kvnl <m.shamsahmar@gmail.com>
2024-05-29 16:29:50 -0400

    ran NS

--- stimulus/cyc04_exp04.py
+++ stimulus/cyc04_exp04.py
@@ -59,7 +59,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'MM'
+subID = 'NS'
 nrep = 5
 nfix = 2  # number of fixation locations
 ndir = 2  # number of direction of motions (flash-left, flash-right)
