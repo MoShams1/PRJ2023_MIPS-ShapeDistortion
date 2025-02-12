ed6162064c39831c77f410cc7ba3f5cf1d5cf224
kvnl <m.shamsahmar@gmail.com>
2025-02-12 11:03:38 -0500

    tested for participant 0011 exp 1 & 2, and also collected their data for exp 1 & exp 2

--- stimulus/cyc05_exp02.py
+++ stimulus/cyc05_exp02.py
@@ -67,7 +67,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'test'
+subID = '0001'
 nrep = 10
 nstm = 2  # number of stimuli (FG, FE)
 nloc = 3  # number of probe locations
