d285cb2a7de014df02364b54eab4c5e1b8cfc003
kvnl <m.shamsahmar@gmail.com>
2025-02-26 11:05:10 -0500

    ran 0003

--- stimulus/cyc05_exp02.py
+++ stimulus/cyc05_exp02.py
@@ -67,7 +67,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = '0004'
+subID = '0003'
 nrep = 10
 nstm = 2  # number of stimuli (FG, FE)
 nloc = 3  # number of probe locations
