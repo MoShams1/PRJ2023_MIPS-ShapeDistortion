b3001182b024dcf8a7dc8d00e30335914edce1b8
kvnl <m.shamsahmar@gmail.com>
2025-03-05 16:20:36 -0500

    0013

--- stimulus/cyc05_exp00_warmUp.py
+++ stimulus/cyc05_exp00_warmUp.py
@@ -64,7 +64,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = '0011'
+subID = '0013-test'
 nrep = 5  # number of repetitions per condition
 nstm = 2  # number of stimuli (FG, FE)
 ndir = 2  # number of direction of motions (flash-left, flash-right)
